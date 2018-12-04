c905e54a667aae2625de362b5ae7da983ef75174
Andrew Barclay <andrewkbarclay@gmail.com>
2018-12-03 18:43:28 -0800

    After carefully re-reading the instructions, checksum have been calculated. Hallelujah!

--- 02a/checksumming.rb
+++ 02a/checksumming.rb
@@ -1,15 +1,30 @@
 ids = File.open('./input.txt').readlines
 
 ids.map! do |id|
-  { id: id, twice: false, thrice: false }
+  { box_id: id, twice: false, thrice: false }
 end
+
+tally = { twice: 0, thrice: 0 }
 
 letters = 'abcdefghijklmnopqrstuvwxyz'.split ''
 
 ids.each do |id|
   letters.each do |letter|
-    # check if 3 in ID
-    # check for 2
-    # if 3 and 2 found, then break
+    if id[:twice] and id[:thrice]
+      break
+    end
+
+    if id[:box_id].count(letter) == 3 and id[:thrice] == false
+      id[:thrice] = true;
+      next
+    elsif id[:box_id].count(letter) == 2 and id[:twice] == false
+      id[:twice] = true;
+      next
+    end
   end
+
+  tally[:twice] += id[:twice] ? 1 : 0
+  tally[:thrice] += id[:thrice] ? 1 : 0
 end
+
+puts "The checksum is #{tally[:twice] * tally[:thrice]}"
